d3c1734d8af174809ad452aa0312f3df2e2b034d
Themir <98977837+themirforget@users.noreply.github.com>
2023-05-26 18:12:06 -0300

    trying to fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
 
-	ssr: {
-		noExternal: ['three', 'troika-three-text']
-	}
+  ssr: {
+    noExternal: ['three', 'troika-three-text']
+  },
+
+  resolve: {
+    alias: {
+      three: resolve('node_modules/three')
+    }
+  }
 });
